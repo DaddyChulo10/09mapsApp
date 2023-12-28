d1c722e3f4a992a7925d6dd1aaec4eb9c5c651ec
DaddyChulo10 <edgarkalim@hotmail.com>
2023-12-28 12:59:38 -0600

    Se creo el script para generar automaticamente el enviroments

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
+
+
 export class SideMenuComponent {
+
+
+
+  public menuItems: MenuItem[] = [
+    { route: '/maps/map/fullscreen', name: 'FullScreen' },
+    { route: '/maps/map/zoom-range', name: 'Zoom Range' },
+    { route: '/maps/map/markers', name: 'Markers' },
+    { route: '/maps/map/properties', name: 'Properties' },
+
+  ]
+
+
 
 }
